b92c790e0aab74c48f1fbeb1bcef606909061e10
Yu Zhang <atomhust@gmail.com>
2015-02-02 21:08:13 -0800

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -10,7 +10,7 @@
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
     
-    [self addObstacle];
+    //[self addObstacle];
 }
 
 -(void)update:(CCTime)delta
